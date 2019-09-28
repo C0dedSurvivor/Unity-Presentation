4d2e0d120cbe13250500b953dd2434552a2d6597
C0dedSurvivor <acingmc@gmail.com>
2019-09-28 15:58:19 -0400

    After Presentation 1: Intro to Unity

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Instantiate(coinPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform myTransform;
+    Rigidbody myRigidbody;
+    public float speed = 0.1f;
+
+    public Text scoreDisplay;
+
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = gameObject.GetComponent<Transform>();
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            myTransform.Translate(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myTransform.Translate(new Vector3(-speed, 0, 0));
+        }
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(transform.position + new Vector3(0, -0.5f, 0), Vector3.down, 0.1f))
+        {
+            myRigidbody.AddForce(new Vector3(0, 150, 0));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            score++;
+            scoreDisplay.text = "Score: " + score;
+        }
+    }
+}
